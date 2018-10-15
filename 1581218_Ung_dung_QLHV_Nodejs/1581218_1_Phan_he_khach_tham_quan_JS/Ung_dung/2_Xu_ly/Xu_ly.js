10c2c45ab3bfbfbc5157a51e6ccc510e3b447925
Nguyễn Đức Hồng <35950600+ndhong2015@users.noreply.github.com>
2018-10-15 19:19:28 +0700

    lấy lại phiên bản ghi đơn xin nghỉ

--- 1581218_Ung_dung_QLHV_Nodejs/1581218_1_Phan_he_khach_tham_quan_JS/Ung_dung/2_Xu_ly/Xu_ly.js
+++ 1581218_Ung_dung_QLHV_Nodejs/1581218_1_Phan_he_khach_tham_quan_JS/Ung_dung/2_Xu_ly/Xu_ly.js
@@ -1,8 +1,5 @@
 ﻿//===== Xử lý Lưu trữ 
 var Dia_chi_Dich_vu = "http://localhost:1000"
-var Dia_chi_Dich_vu_Quan_ly = "http://localhost:3000"
-var Dia_chi_Dich_vu_Sinh_vien = "http://localhost:7000"
-var Dia_chi_Dich_vu_Giao_vien = "http://localhost:4000"
 var Dia_chi_Dich_vu_Media = "http://localhost:2000"
 function Doc_Danh_sach_Lop() {
     var Danh_sach = []
@@ -44,42 +41,7 @@ function Doc_Danh_sach_Giao_vien() {
     Danh_sach = JSON.parse(Chuoi)
     return Danh_sach
 }
-function Dang_nhap(Tai_khoan) {
-    var Doi_tuong = []
-    var Chuoi_Goi = JSON.stringify(Tai_khoan)
-    var Xu_ly_HTTP = new XMLHttpRequest()
 
-    var Danh_sach_Sinh_vien = Doc_Danh_sach_Sinh_vien()
-    var Danh_sach_Quan_ly = Doc_Danh_sach_Quan_ly()
-    var Danh_sach_Giao_vien = Doc_Danh_sach_Giao_vien()
-    var Dia_chi = ""
-    if (Danh_sach_Quan_ly.some(x => x.Ten_dang_nhap == Ten_dang_nhap
-        && x.Mat_khau == Mat_khau )) {
-            window.location.href ="../../../1581218_2_Phan_he_quan_ly_JS/Ung_dung/1_Giao_dien/MH_Quan_ly.html?"+"Th_Ten_dang_nhap=" + Ten_dang_nhap +"&Th_Mat_khau="+Mat_khau                                      
-            Dia_chi = `${Dia_chi_Dich_vu_Quan_ly}?Ma_so_Xu_ly=Dang_nhap`
-    }
-    else if (Danh_sach_Sinh_vien.some(x => x.Ten_dang_nhap == Ten_dang_nhap
-            && x.Mat_khau == Mat_khau )) {
-            window.location.href ="../../../1581218_3_Phan_he_sinh_vien_JS/Ung_dung/1_Giao_dien/MH_Sinh_vien.html?"+"Th_Ten_dang_nhap=" + Ten_dang_nhap +"&Th_Mat_khau="+Mat_khau                                      
-            Dia_chi = `${Dia_chi_Dich_vu_Sinh_vien}?Ma_so_Xu_ly=Dang_nhap`
-    } 
-    else if (Danh_sach_Giao_vien.some(x => x.Ten_dang_nhap == Ten_dang_nhap
-            && x.Mat_khau == Mat_khau )) {
-            window.location.href ="../../../1581218_4_Phan_he_giao_vien_JS/Ung_dung/1_Giao_dien/MH_Giao_vien.html?"+"Th_Ten_dang_nhap=" + Ten_dang_nhap +"&Th_Mat_khau="+Mat_khau                                       
-            Dia_chi = `${Dia_chi_Dich_vu_Giao_vien}?Ma_so_Xu_ly=Dang_nhap`
-    }                      
-    else {
-    Th_Thong_bao.innerHTML="<div>Đăng nhập không hợp lệ" 
-    + "<br />Xin vui lòng <b>Nhập lại</b> Tên đăng nhập và Mật khẩu" + "</div>"
-    }  
-
-    //var Dia_chi = `${Dia_chi_Dich_vu}?Ma_so_Xu_ly=Dang_nhap`
-    Xu_ly_HTTP.open("POST", Dia_chi, false)
-    Xu_ly_HTTP.send(Chuoi_Goi)
-    var Chuoi_Kq = Xu_ly_HTTP.responseText
-    Doi_tuong = JSON.parse(Chuoi_Kq)
-    return Doi_tuong
-}
 //===== Xử lý Thể hiện
 function Tao_Chuoi_HTML_Khach_Tham_quan_Khi_Chao(Khach_Tham_quan) {
     var Dia_chi = `${Dia_chi_Dich_vu_Media}/icon_khach_tham_quan.png`
